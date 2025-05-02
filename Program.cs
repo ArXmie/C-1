6588f6ed43cbb6c13dfb7ec30d188dc9a369baed
Студент группы IS <is@sielom.local>
2025-05-02 16:40:31 +0500

    a

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace RuhArteam;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] massive = [ 1, 1, 2 ];
+        int uniquenum = 0;
+        int[] massive_result = new int[] {};
+
+        for (int i = 0; i < massive.Length; i++)
+        {
+            for (int j = 0; j < massive.Length; j++){
+                if (i < massive.Length - 1 && massive[i] == massive[j]) {
+                    massive_result[i - 1] = massive[i];
+                    uniquenum++;
+                }
+            }
+        }
+        Console.WriteLine(uniquenum);
+        
+    }
+}
